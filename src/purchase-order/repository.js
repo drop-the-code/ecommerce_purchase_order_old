fde18d2584d2cb014ab8bb028153e5aec065f09c
Carlos Alberto <carloshtcursos@gmail.com>
2021-06-15 00:03:39 -0400

    adicionando dados do schema

--- src/purchase-order/repository.js
+++ src/purchase-order/repository.js
@@ -1,6 +1,25 @@
 const { Schema, model } = require('mongoose');
 
-const getSchemaDefinition = () => ({});
+const getSchemaDefinition = () => ({
+  total_price: { type: Schema.Types.Number, required: true, default: 0.0 },
+  cliente: {
+    id: { type: Schema.Types.Number, required: true, default: 1 },
+    cpf: { type: Schema.Types.String, required: true, default: null },
+  },
+  products: [
+    {
+      id: { type: Schema.Types.Number, required: true, default: 1 },
+      price: { type: Schema.Types.Number, required: true, default: 1 },
+      name: { type: Schema.Types.String, required: true, default: null },
+      product_supplier_cnpj: {
+        type: Schema.Types.String,
+        required: true,
+        default: null,
+      },
+      description: { type: Schema.Types.String, required: true, default: null },
+    },
+  ],
+});
 
 const getSchema = (schemaDefinition) =>
   new Schema(schemaDefinition, { timestamps: true });
